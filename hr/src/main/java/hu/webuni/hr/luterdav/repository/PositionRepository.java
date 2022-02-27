0199087337d35c733db7486ff5481d89c06e8965
luterdav <78601699+luterdav@users.noreply.github.com>
2022-02-27 22:21:08 +0100

    6. heti házi

--- hr/src/main/java/hu/webuni/hr/luterdav/repository/PositionRepository.java
+++ hr/src/main/java/hu/webuni/hr/luterdav/repository/PositionRepository.java
@@ -1,11 +1,13 @@
 package hu.webuni.hr.luterdav.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hu.webuni.hr.luterdav.model.Position;
 
 public interface PositionRepository extends JpaRepository<Position, Long>{
 	
-	//List<Company> countBySalaryGreaterThan(Integer salary);
+	public List<Position> findByName(String name);
 
 }
